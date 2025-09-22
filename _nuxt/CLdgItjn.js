defd0ddbea5494f7cc896244a6653e4e499d13c4
M3D3L <72050286+M3D3L@users.noreply.github.com>
2025-09-22 02:53:55 +0000

    Deploying to gh-pages from @ M3D3L/common@286ca155ba85db496cad7b3910d9e22d28d3e7c2 🚀

--- _nuxt/CLdgItjn.js
+++
@@ -1,6 +0,0 @@
-import{c as s}from"./DiNFcdZn.js";import{c as t,j as c,o as e,F as l,r as o,f as n,w as i,i as f,u as m,g as u,n as k}from"#entry";/**
- * @license lucide-vue-next v0.488.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const v=s("linkedin",[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z",key:"c2jq9f"}],["rect",{width:"4",height:"12",x:"2",y:"9",key:"mk3on5"}],["circle",{cx:"4",cy:"4",r:"2",key:"bt5ra8"}]]),x={__name:"Socials",props:{socialLinks:{type:Array,default:()=>[]},columnOnMobile:{type:Boolean,default:!1}},setup(a){return(h,d)=>a.socialLinks?(e(),t("div",{key:0,class:k([{"flex-col lg:flex-row":a.columnOnMobile},"flex gap-2 mt-3"])},[(e(!0),t(l,null,o(a.socialLinks,r=>(e(),n(m(u),{key:r.href,variant:"default",size:"icon",as:"a",href:r.href,target:"_blank",class:"hover:scale-105 trasform"},{default:i(()=>[(e(),n(f(r.icon),{class:"w-5 h-5"}))]),_:2},1032,["href"]))),128))],2)):c("",!0)}};export{v as L,x as _};
