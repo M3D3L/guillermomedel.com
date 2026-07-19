463be4225cfd9a72d4fd896d2d1f712da80cb39e
M3D3L <72050286+M3D3L@users.noreply.github.com>
2026-07-19 01:04:21 +0000

    Deploying to gh-pages from @ M3D3L/common@7bea4ab041fe8b9c055cf3e3bd4c49bf49621690 🚀

--- _nuxt/f2Xo2d3O.js
+++
@@ -1,6 +0,0 @@
-import{c as a}from"./DbYJuBnD.js";import{e as c,c as n,o,q as t,u as r}from"#entry";import{c as p}from"./DmrNP6qg.js";const f=c({__name:"Skeleton",props:{class:{}},setup(e){const s=e;return(m,l)=>(o(),n("div",{class:t(r(a)("animate-pulse rounded-md bg-primary",s.class))},null,2))}});/**
- * @license lucide-vue-next v0.488.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const _=p("utensils",[["path",{d:"M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2",key:"cjf0a3"}],["path",{d:"M7 2v20",key:"1473qp"}],["path",{d:"M21 15V2a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7",key:"j28e5"}]]);export{_ as U,f as _};
