87ce479d5e223517f9fa83ba3a7d74bb9f54e151
M3D3L <72050286+M3D3L@users.noreply.github.com>
2026-08-05 05:53:30 +0000

    Deploying to gh-pages from @ M3D3L/common@47ba9640d97e35c43823f5255398c3a7256bf83f 🚀

---
+++ _nuxt/B_2TQqee.js
@@ -0,0 +1,11 @@
+import{c as a}from"./vV6c201n.js";/**
+ * @license lucide-vue-next v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=a("send",[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]]);/**
+ * @license lucide-vue-next v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=a("shopping-bag",[["path",{d:"M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z",key:"hou9p0"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M16 10a4 4 0 0 1-8 0",key:"1ltviw"}]]);export{t as S,e as a};
